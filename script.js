4c33168dddabb7240252d684bfeb8d70e6aa557a
Daniel Clarke <daniel@danielrlc.com>
2021-01-03 22:35:43 +0200

    wip - rendering free-to-view article

--- script.js
+++ script.js
@@ -1,6 +1,4 @@
-fetch(
-  'http://localhost:3001/',
-)
+fetch('http://localhost:3001/')
   .then(function (response) {
     if (response.ok) {
       return response.json();
@@ -10,6 +8,38 @@ fetch(
   })
   .then(function (data) {
     console.log(data);
+    var app = document.querySelector('#app');
+    var title = document.createElement('h1');
+    title.textContent = data.title;
+    app.appendChild(title);
+    data.body.map((element) => {
+      if (element.html) {
+        var p = document.createElement('p');
+        p.innerHTML = element.html;
+        app.appendChild(p);
+      } else if (element.headline) {
+        var h2 = document.createElement('h2');
+        h2.textContent = element.headline;
+        app.appendChild(h2);
+      } else if (element.image) {
+        var img = document.createElement('img');
+        img.src = element.image.url;
+        img.alt = element.image.caption;
+        app.appendChild(img);
+      } else if (element.box) {
+        var h2 = document.createElement('h2');
+        h2.textContent = element.box.headline;
+        app.appendChild(h2);
+        var h3 = document.createElement('h3');
+        h3.textContent = element.box.title;
+        app.appendChild(h3);
+        element.box.content.map((contentItem) => {
+          var p = document.createElement('p');
+          p.innerHTML = contentItem;
+          app.appendChild(p);
+        });
+      }
+    });
   })
   .catch(function (error) {
     console.warn('Something went wrong.', error);
